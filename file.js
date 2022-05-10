d59a334923bc8bb356591efab90ab0d901310bf4
Faith Muthoni <faith@Faiths-MacBook-Pro.local>
2022-05-10 19:24:22 +0300

    alerts the totals of inputs

--- file.js
+++ file.js
@@ -4,6 +4,11 @@ function getinputval(){
     var lname = document.getElementById('lname').value;
 //    document.write(fname)
     // alert(fname);
-    document.getElementById("paragraph").innerHTML = 'Your First name is  ' + fname + '.';
-    document.getElementById("paragraphh").innerHTML = 'Your Last name is  ' + lname + '.';
+    // document.getElementById("paragraph").innerHTML = 'Your First name is  ' + fname + '.';
+    // document.getElementById("paragraphh").innerHTML = 'Your Last name is  ' + lname + '.';
+    var a = parseInt(fname)
+    var b = parseInt(lname)
+    var sumation = a+b
+
+    alert (sumation)
 }
