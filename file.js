dc9750c4a04dadafa49e2d7f86c867abcfaef75e
Faith Muthoni <faith@Faiths-MacBook-Pro.local>
2022-05-16 19:54:44 +0300

    added the front end of my web

--- file.js
+++ file.js
@@ -1,22 +1,30 @@
+var yourSelect = document.getElementById("pickedroom"); 
+var numberofrooms =  parseFloat(document.getElementsByClassName("numroom").value);
 
-function getinputval(){
-    var fname = document.getElementById('fname').value;
-    var lname = document.getElementById('lname').value;
-//    document.write(fname)
-    // alert(fname);
-    // document.getElementById("paragraph").innerHTML = 'Your First name is  ' + fname + '.';
-    // document.getElementById("paragraphh").innerHTML = 'Your Last name is  ' + lname + '.';
-    var a = parseInt(fname)
-    var b = parseInt(lname)
-    var sumation = a+b
+ 
+function  getvalue(){
+    // prevents reloading after getting data 
+    event.preventDefault();
+   
+    var roompicked;
+     room =  yourSelect.options[ yourSelect.selectedIndex].text;
+    if (room == 'DeluxeA'){
+        totalcost = numberofrooms*15000;
+        document.getElementById('paragraph').innerHTML = "Pay " + totalcost;   
+    } 
+    else if (room == 'DeluxeB'){
+        totalcost = numberofrooms*15000;
+        alert(totalcost)
+        document.getElementById('paragraph').innerHTML = "pay " + totalcost; 
+    }
+    else if (room == 'DeluxeC'){
+        totalcost = numberofrooms*15000;
+        document.getElementById('paragraph').innerHTML = "pay " + totalcost;  
+    }else{
+        totalcost = numberofrooms*10000;
+        alert(totalcost)
+        document.getElementById('paragraph').innerHTML = "pay " + totalcost;  
+    } 
 
-    alert (sumation)
 }
-function Person(first,last,favcolor,country){
-    this.firstname = first;
-    this.lastname = last;
-    this.favouritecolor = favcolor;
-    this.countryofbirth = country;
-
-}
-const paul = new Person ('paul','ndemo','blue','kenya');
+  
