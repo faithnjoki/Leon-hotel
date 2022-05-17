729310c4a2963de8e78a05a7c4ecb5eb2e04bee3
Faith Muthoni <faith@Faiths-MacBook-Pro.local>
2022-05-17 09:49:32 +0300

    worked on input type number

--- file.js
+++ file.js
@@ -1,28 +1,25 @@
 var yourSelect = document.getElementById("pickedroom"); 
-var numberofrooms =  parseFloat(document.getElementsByClassName("numroom").value);
+var numberInput = document.querySelector("input[type='number']");
 
- 
+
+
+
 function  getvalue(){
     // prevents reloading after getting data 
     event.preventDefault();
-   
-    var roompicked;
-     room =  yourSelect.options[ yourSelect.selectedIndex].text;
-    if (room == 'DeluxeA'){
-        totalcost = numberofrooms*15000;
+    // get value in options 
+    roomname =  yourSelect.options[ yourSelect.selectedIndex].text;
+    var totalrooms;
+    totalrooms = numberInput.valueAsNumber;    
+
+    if (roomname == 'DeluxeA' || roomname == 'DeluxeB'  || roomname == 'DeluxeC' ){
+        totalcost = totalrooms*15000;
+        
         document.getElementById('paragraph').innerHTML = "Pay " + totalcost;   
     } 
-    else if (room == 'DeluxeB'){
-        totalcost = numberofrooms*15000;
-        alert(totalcost)
-        document.getElementById('paragraph').innerHTML = "pay " + totalcost; 
-    }
-    else if (room == 'DeluxeC'){
-        totalcost = numberofrooms*15000;
-        document.getElementById('paragraph').innerHTML = "pay " + totalcost;  
-    }else{
-        totalcost = numberofrooms*10000;
-        alert(totalcost)
+    else{
+        totalcost =  totalrooms*10000;
+        console.log(totalcost)
         document.getElementById('paragraph').innerHTML = "pay " + totalcost;  
     } 
 
