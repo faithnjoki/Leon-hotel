6ded39e8a64defa2b41d7fbc5e1146592826af51
Faith Muthoni <faith@Faiths-MacBook-Pro.local>
2022-05-17 12:01:12 +0300

    edit js file

--- file.js
+++ file.js
@@ -1,25 +1,19 @@
 var yourSelect = document.getElementById("pickedroom"); 
-var numberInput = document.querySelector("input[type='number']");
-
-
-
 
 function  getvalue(){
     // prevents reloading after getting data 
     event.preventDefault();
     // get value in options 
     roomname =  yourSelect.options[ yourSelect.selectedIndex].text;
-    var totalrooms;
-    totalrooms = numberInput.valueAsNumber;    
+    var numberInput = parseInt(document.querySelector(".numroom"));
 
     if (roomname == 'DeluxeA' || roomname == 'DeluxeB'  || roomname == 'DeluxeC' ){
-        totalcost = totalrooms*15000;
-        
+        totalcost = numberInput*15000;
+        console.log(totalcost)
         document.getElementById('paragraph').innerHTML = "Pay " + totalcost;   
     } 
     else{
-        totalcost =  totalrooms*10000;
-        console.log(totalcost)
+        totalcost =  numberInput*10000;
         document.getElementById('paragraph').innerHTML = "pay " + totalcost;  
     } 
 
