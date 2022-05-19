bb8dfdb0be980d45cd8bb3a918b00aaceb5b62bf
Faith Muthoni <faith@Faiths-MacBook-Pro.local>
2022-05-19 13:18:24 +0300

    add conditions

--- file.js
+++ file.js
@@ -1,21 +1,53 @@
-var yourSelect = document.getElementById("pickedroom"); 
+
 
 function  getvalue(){
+    var yourSelect = document.getElementById("pickedroom"); 
+    roomname =  yourSelect.options[ yourSelect.selectedIndex].text;
     // prevents reloading after getting data 
     event.preventDefault();
     // get value in options 
-    roomname =  yourSelect.options[ yourSelect.selectedIndex].text;
-    var numberInput = parseInt(document.querySelector(".numroom"));
+    var numberInput = parseInt(document.querySelector(".numroom").value);
 
-    if (roomname == 'DeluxeA' || roomname == 'DeluxeB'  || roomname == 'DeluxeC' ){
+    if (roomname === 'DeluxeA' ){
+        var numberInput = parseInt(document.querySelector(".numroom").value);
         totalcost = numberInput*15000;
         console.log(totalcost)
         document.getElementById('paragraph').innerHTML = "Pay " + totalcost;   
     } 
-    else{
-        totalcost =  numberInput*10000;
-        document.getElementById('paragraph').innerHTML = "pay " + totalcost;  
-    } 
+    else if (roomname ==='DeluxeB'){
+        var numberInput = parseInt(document.querySelector(".numroom1").value);
+        totalcost = numberInput*15000;
+        console.log(totalcost)
+        document.getElementById('paragraph').innerHTML = "Pay " + totalcost;
 
-}
+    }
+    else if (roomname ==='DeluxeC'){
+        var numberInput = parseInt(document.querySelector(".numroom2").value);
+        totalcost = numberInput*15000;
+        console.log(totalcost)
+        document.getElementById('paragraph').innerHTML = "Pay " + totalcost;
+
+    }
+    else if (roomname ==='EnsuiteA'){
+        var numberInput = parseInt(document.querySelector(".numroom3").value);
+        totalcost = numberInput*10000;
+        console.log(totalcost)
+        document.getElementById('paragraph').innerHTML = "Pay " + totalcost;
+
+    }
+    else if (roomname ==='EnsuiteB'){
+        var numberInput = parseInt(document.querySelector(".numroom4").value);
+        totalcost = numberInput*10000;
+        console.log(totalcost)
+        document.getElementById('paragraph').innerHTML = "Pay " + totalcost;
+
+    }
+    else{
+        var numberInput = parseInt(document.querySelector(".numroom5").value);
+        totalcost = numberInput*10000;
+        console.log(totalcost)
+        document.getElementById('paragraph').innerHTML = "Pay " + totalcost;
+
+    } 
+} 
   
